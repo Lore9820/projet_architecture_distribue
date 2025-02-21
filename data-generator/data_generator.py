aaccfaceda94baa35a78bf52acf1017dca490942
Clement Tetard <clement.tetard@gmail.com>
2025-02-21 14:47:53 +0100

    cleaning data-generator

--- data-generator/data_generator.py
+++ data-generator/data_generator.py
@@ -1,21 +1,9 @@
 import time
 import random
-from datetime import datetime
 from kafka import KafkaProducer
-import os
-
 
 # Configuration du producteur Kafka
 producer = KafkaProducer(bootstrap_servers="kafka:9092")
-
-# Read the log file
-cwd = os.getcwd()
-print(cwd)
-
-# Generate a line from the log file
-def generate_log():
-    x = random.randint(0, len(logs_df.collect()))
-    return logs_df[x].value
 
 log_file = "./web_server.log"
 f = open(log_file, "r")
