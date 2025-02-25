5934602fb8ae907dd6797c64ee595efc9e8527ab
Lore Goethals <lore.goethals2@etu.univ-lorraine.fr>
2025-02-25 10:07:26 +0100

    Addition of right job to streamjob2

--- apps/stream_job2.py
+++ apps/stream_job2.py
@@ -3,7 +3,7 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
 # Initialisation de la session Spark
-spark = SparkSession.builder.appName("StreamJob1").config("spark.mongodb.output.uri", "mongodb://mongo:27017/log.streamjob1").getOrCreate()
+spark = SparkSession.builder.appName("StreamErreur").config("spark.mongodb.output.uri", "mongodb://mongo:27017/log.streamErreur").getOrCreate()
 
 # Lecture des logs depuis Kafka
 kafka_df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "kafka:9092").option("subscribe", "log").load()
@@ -22,14 +22,18 @@ parsed_logs = logs_df.withColumn("log_parts", split(col("value"), " ")).select(
         col("log_parts")[9].cast("int").alias("size")  # Taille de la réponse
     )
 
-# Agrégation des logs par code HTTP
-status_counts = parsed_logs.groupBy("status").count()
+# Agrégation des logs par code HTTP et intervalle de temps
+streamErreur = parsed_logs.filter(col("status").isin(404, 500)).groupBy(window(col("timestamp"), "5 minutes"), "status").count().filter(col("count") > 10)
 
 # Fonction pour écrire dans MongoDB (sans écraser)
 def write_to_mongo(df, epoch_id):
-    df.write.format("mongo").mode("append").option("replaceDocument", "false").save()
+    # Convertir la colonne window en format lisible
+    df_formatted = df.withColumn("start_time", col("window.start")).withColumn("end_time", col("window.end")).drop("window")
+    
+    # Écrire dans MongoDB
+    df_formatted.write.format("mongo").mode("append").option("replaceDocument", "false").save()
 
 # Écriture des résultats dans MongoDB en streaming
-query = status_counts.writeStream.outputMode("update").foreachBatch(write_to_mongo).start()
+query = streamErreur.writeStream.outputMode("update").foreachBatch(write_to_mongo).start()
 
 query.awaitTermination()
